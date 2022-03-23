eeddcee6daa94ca37a1f9aea67ba77de94163546
Phil Kieti <philmkieti@hotmail.com>
2022-03-22 21:07:44 -0400

    Cart Component Fully implemented

--- README react-redux-tutorial 2022-3-17.txt
+++ README react-redux-tutorial 2022-3-17.txt
@@ -1,3 +1,37 @@
+2022-3-20
+
+End of Cart functions
+1:49:20 / 4:39:31
+Part 07- Cart Component (State Management)
+
+2022-3-18
+
+Use rcc to create classes
+
+1. Set Active Task management spredsheet
+2. Create cart-component branch
+3. Product.js
+4. Handle 'Add to Cart' to this.props.addToCart(product)
+5. App.js
+6. Add cartItems to state as []
+7. Create addToCart(product)
+8. Slice, Check product existence, add to cartItems
+10. Define cartItems, order from this.props
+11. Check cartItems.length and show message
+12. List cartItems {cartItems.length > 0 && ()}
+13. index.css
+14. Style cart, cart-header, cart-items (img, li),
+15. Use LocalStorage on App constructor to load cart items (JSON.parse)
+16. Use LocalStorage on addToCart to save cart items (JSON.stringify)
+
+1:25:36 / 4:39:31
+Part 07- Cart Component (State Management)
+#React #Redux #Tutorial
+React & Redux Tutorial - Full Shopping Cart in 4 Hours [2020]
+114,673 viewsJul 13, 2020
+https://www.youtube.com/watch?v=nKyrXWH5XLM&t=264s
+
+$$$$$$$$$$$$$$
 https://www.youtube.com/watch?v=nKyrXWH5XLM&t=264s
 
 4:28 / 4:39:31
@@ -9,17 +43,6 @@ Part 01- What You Will Make
 #React #Redux #Tutorial
 React & Redux Tutorial - Full Shopping Cart in 4 Hours [2020]
 114,673 viewsJul 13, 2020
-
-2.4K
-
-DISLIKE
-
-SHARE
-
-CLIP
-
-SAVE
-
 
 Coding with Basir
 37K subscribers
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// feature 1
 import React from 'react';
+import Cart from './components/Cart';
 import Filter from './components/Filter';
-// import './App.css';
 import Products from './components/Products';
 import data from './data.json';
 
@@ -10,13 +9,35 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: '',
       sort: ''
     };
   }
 
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter(x => x._id !== product._id)
+    });
+  }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems})
+  }
+
   filterProducts = (event) => {
-    // impl
     // console.log(event.target.value);
     if (event.target.value === '') {
       this.setState({ size: event.target.value, products: data.products });
@@ -32,7 +53,6 @@ class App extends React.Component {
 
   // sortProducts(event) {
   sortProducts = (event) => { // converto to method!
-    // implement
     const sort = event.target.value;
     // console.log(event.target.value);
     this.setState((state) => ({
@@ -71,10 +91,10 @@ class App extends React.Component {
                 sort={this.state.sort}
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts} />
-              <Products products={this.state.products} />
+              <Products products={this.state.products} addToCart={this.addToCart} />
             </div>
             <div className='sidebar'>
-              Cart Items
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
             </div>
           </div>
         </main>
